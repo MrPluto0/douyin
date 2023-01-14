15eb720049b96639a9d52e1fcfe1885bb612e0e3
Gypsophlia <710852553@qq.com>
2023-01-14 16:17:00 +0800

    feat: init

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Request struct {
+	Username string `json:"username" binding:"required"`
+	Nickname string `json:"nickname" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+	Age      uint8  `json:"age" binding:"gte=1,lte=120"`
+}
+
+func main() {
+	server := gin.Default()
+	server.GET("/ping", func(ctx *gin.Context) {
+		var req Request
+		err := ctx.BindJSON(&req)
+		if err != nil {
+			ctx.JSON(http.StatusOK, gin.H{"msg": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusOK, req)
+	})
+	server.Run()
+}
